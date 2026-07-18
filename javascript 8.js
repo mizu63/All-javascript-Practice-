57f58b90d88340761c076841b5a182234cc271c4
Md. Mizu Ahmmed Jim <mdmizu634431@gmail.com>
2026-07-18 18:03:12 +0600

    pretices

--- javascript 8.js
+++ javascript 8.js
@@ -1,43 +1,87 @@
-//1.Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
-let price1=1000;
-let price2="1000";
-console.log(price1==price2);
-console.log(price1===price2);
-console.log(price1!=price2);
-console.log(price1!==price2);
-console.log(price1>price2);
-console.log(price1<price2);
-console.log(price1>=price2);
-console.log(price1<=price2);
+// //1.Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+// let price1=1000;
+// let price2="1000";
+// console.log(price1==price2);
+// console.log(price1===price2);
+// console.log(price1!=price2);
+// console.log(price1!==price2);
+// console.log(price1>price2);
+// console.log(price1<price2);
+// console.log(price1>=price2);
+// console.log(price1<=price2);
 
-//2.Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
- let a="5";
- let b=5;
- console.log(a==b); //true
- console.log(a===b); //false
+// //2.Guess the result of "5" == 5 and "5" === 5 first, then write code to verify your guess.
+//  let a="5";
+//  let b=5;
+//  console.log(a==b); //true
+//  console.log(a===b); //false
 
-//3.Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+// //3.Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
 
-let  isRaining=true;
+// let  isRaining=true;
 
-if( isRaining){
-    console.log("Take an umbrella")
-}
+// if( isRaining){
+//     console.log("Take an umbrella")
+// }
 
-//4.Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
+// //4.Take a variable stock. If stock is 0, print "Out of stock" (use only if, no else).
 
-let stock=0;
-if(stock==0){
-    console.log("out of stock")
-}
+// let stock=0;
+// if(stock==0){
+//     console.log("out of stock")
+// }
 
-//5.Take a number variable and check whether it is positive or negative (using if-else).
+// //5.Take a number variable and check whether it is positive or negative (using if-else).
 
-let money=-10;
+// let money=-10;
 
-if(money>0){
-    console.log("positive")
-}
-else{
-    console.log("negative")
-}
+// if(money>0){
+//     console.log("positive")
+// }
+// else{
+//     console.log("negative")
+// }
+//6.Take a year variable and check whether it is a leap year (hint: year % 4 === 0).
+
+// let year=2012;
+// if(year%4===0){
+//     console.log("leap year")
+// }
+// else{
+//     console.log("Not a leap yeatr")
+// }
+//7.Take a speed variable. If speed is greater than 80, print "Overspeeding", otherwise print "Normal speed".
+
+// let speed=80;
+
+// if(speed>80){
+//     console.log("Overspeeding")
+// }
+// else{
+//      console.log("Normal speed")
+// }
+//8.Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+// let age=21;
+// let hasTicket=true;
+// if(age>18 && hasTicket){
+//     console.log("Entry allowed")
+// }
+// else{
+//      console.log(" Not Entry allowed")
+// }
+//9.Using isWeekend and isHoliday, print "No work today" if either one is true (use ||).
+
+// let isWeekend=true;
+// let isHoliday=false;
+// if(isHoliday || isWeekend ){
+//     console.log("No work today")
+// }
+//10.Build a simple login system — print "Login successful" only if both username and password are correct.
+
+// let username = "admin";
+// let password = "123456";
+
+// if (username === "admin" && password === "123456") {
+//     console.log("Login successful");
+// }
